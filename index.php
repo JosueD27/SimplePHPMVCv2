0e99942e7e8a182c42614c3a17d767480c72c5ae
Josué <josueamador2711@gmail.com>
2020-11-03 19:46:53 -0600

    Categorias Filtro

--- index.php
+++ index.php
@@ -99,13 +99,13 @@ case "programa":
       include_once "controllers/security/programa.control.php":
       mw_redirectToLogin($_SERVER["QUERY_STRING"]);
     die();
-    
-case "categorias"
+
+case "categorias":
     ($logged)?
       include_once "controllers/mnt/categorias.control.php":
       mw_redirectToLogin($_SERVER["QUERY_STRING"]);
     die();    
-case "categoria"
+case "categoria":
     ($logged)?
       include_once "controllers/mnt/categoria.control.php":
       mw_redirectToLogin($_SERVER["QUERY_STRING"]);
